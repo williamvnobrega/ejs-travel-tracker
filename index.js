dc75137b62b83c9e211f71b95af212072c5489dd
williamvnobrega <williamvnobrega@gmail.com>
2024-04-03 17:13:33 -0300

    implement error handler to improve user experience

--- index.js
+++ index.js
@@ -40,15 +40,31 @@ app.get("/", async (req, res) => {
 app.post("/add", async(req,res)=>{
   const input = req.body["country"];
 
-  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1",[input]);
-
-  if(result.rows.length !== 0){
+  try{
+    const result = await db.query("SELECT country_code FROM countries WHERE LOWER (country_name) LIKE '%' || $1 '%';",[input.toLowerCase()]);
     const data = result.rows[0];
     const countryCode = data.country_code;
-
-    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",[countryCode,])
-  }
-  res.redirect("/");
+    try{
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",[countryCode,]); //<- here
+      res.redirect("/");
+    } catch(err){
+      console.log(err);
+      const countries = await checkVisited();
+      res.render("index.ejs",{
+        countries:countries,
+        total:countries.length,
+        error:"Country has already been added, try again.",
+      })
+    }
+  }catch(err){
+    console.log(err);
+    const countries = await checkVisited();
+    res.render("index.ejs",{
+      countries: countries,
+      total:countries.length,
+      error:"Country name does not exist, try again. ",
+    });
+  }  
 })
 
 app.listen(port, () => {
